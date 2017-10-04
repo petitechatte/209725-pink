fd1aa886be2f2c745cc683f5ddbb3ab4a31a7a1a
Natalya Kot <natalyavaskot@gmail.com>
2017-10-04 18:58:16 +0300

    Реализовано интерактивное меню

---
+++ js/menu.js
@@ -0,0 +1,15 @@
+var header = document.querySelector(".page-header");
+var menu = document.querySelector(".main-nav");
+var menuToggle = document.querySelector(".main-nav__toggle");
+
+header.classList.remove("page-header--stretched");
+menu.classList.remove("main-nav--opened");
+menuToggle.classList.add("main-nav__toggle--open");
+
+menuToggle.addEventListener("click", function (evt) {
+  evt.preventDefault;
+  header.classList.toggle("page-header--stretched");
+  menu.classList.toggle("main-nav--opened");
+  menuToggle.classList.toggle("main-nav__toggle--open");
+  menuToggle.classList.toggle("main-nav__toggle--close");
+});
